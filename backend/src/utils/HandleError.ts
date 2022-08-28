401c2f1ed7cc30634fe6a79bae800537cfd1c509
Hugoacr <hugoacr@yahoo.com.br>
2022-08-27 23:17:34 -0300

    chore(utils): cria HandleError

---
+++ backend/src/utils/HandleError.ts
@@ -0,0 +1,24 @@
+class HandleError {
+    badRequest(message: string) {
+      return ({
+        message,
+        status: 400,
+      });
+    }
+  
+    unauthorized(message: string) {
+      return ({
+        message,
+        status: 401,
+      });
+    }
+  
+    notFound(message: string) {
+      return ({
+        message,
+        status: 404,
+      });
+    }
+  }
+  
+  export default new HandleError();
